36c6362dc3bf162a9d7591ecd7f91319e12f8b6a
gaoyongxiao <gaoyongxiao@meituan.com>
2018-05-30 09:44:10 +0800

    use leaf provider

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,15 +1,32 @@
 import Vapor
+import Leaf
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    
+    router.get("name") { req in
+        return "Passaction"
     }
+    
+    router.get("age") { req in
+        return 23
+    }
+    
+    router.get("json") { req in
+        return Person(name: "Passaction", age: 23)
+    }
+    
+    router.get("view") { req -> Future<View> in
+        return try req.view().render("welcome")
+    }
+    
+    router.get("bonus") { (req) -> Future<View> in
+        let passaction = Person(name: "Passaction", age: 23)
+        return try req.view().render("whoami", passaction)
+    }
+}
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+struct Person: Content {
+    var name: String
+    var age: Int
 }
