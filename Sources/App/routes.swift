da1daa43273baa970beff8f7314049a74f33b544
gaoyongxiao <gaoyongxiao@meituan.com>
2018-05-31 10:16:48 +0800

    split route functions to controller

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,20 +3,11 @@ import Leaf
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    router.get("users") { req -> Future<View> in
-        return User.query(on: req).all().flatMap({ users in
-            let data = ["userlist": users]
-            return try req.view().render("userview", data)
-        })
-    }
     
-    router.post("users") { req -> Future<Response> in
-        return try req.content.decode(User.self).flatMap({ user in
-            return user.save(on: req).map({ _ in
-                return req.redirect(to: "users")
-            })
-        })
-    }
+    let userController = UserController()
+    router.get("users", use: userController.list)
+    router.post("users", use: userController.create)
+    
 }
 
 struct Person: Content {
